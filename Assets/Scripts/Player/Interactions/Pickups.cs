214562b84c23b2670b9e644f4b717c04d1955354
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2023-11-11 12:41:18 +0200

    prefab update

--- Assets/Scripts/Player/Interactions/Pickups.cs
+++ Assets/Scripts/Player/Interactions/Pickups.cs
@@ -1,15 +1,19 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Player.Interactions
 {
     public class Pickups : MonoBehaviour
     {
+        [SerializeField] private PlayerInfo pI;
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Drug"))
             {
-                Debug.Log("+1 Drug");
+                var rand = Random.Range(1, 5);
+                pI.drugs += rand;
+                Debug.Log("+" + rand + " Drugs");
                 Destroy(other.gameObject);
             }
         }
---
+++ Assets/Scripts/Player/Interactions/PlayerInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Player.Interactions
+{
+    public class PlayerInfo : MonoBehaviour
+    {
+        public int drugs;
+        public int hp;
+        [SerializeField] private int maxHP;
+
+        private void Awake()
+        {
+            drugs = 0;
+            hp = maxHP;
+        }
+    }
+}

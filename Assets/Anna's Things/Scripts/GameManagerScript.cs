57139badf5d5c66bdd62ec71a51ef55159186114
Annayingyang <2540640@students.wits.ac.za>
2023-11-12 21:59:01 +0200

    UI Imported

--- Assets/Anna's Things/Scripts/GameManagerScript.cs
+++ Assets/Anna's Things/Scripts/GameManagerScript.cs
@@ -1,41 +1,29 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public GameObject ruleMenu;
-    public GameObject mainMenu;
-    public GameObject panel;
-    public KeyCode pause = KeyCode.Escape;
 
-    private void Update()
+
+
+    public GameObject ruleMenu;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Input.GetKeyDown(pause))        
-        {                                   
-            if (panel.activeSelf)                      
-            {                               
-               panel.SetActive(false);
-               Debug.Log("active");
-               Cursor.lockState = CursorLockMode.Locked;
-               Cursor.visible = false;
-            }                               
-            else                            
-            {      
-                Debug.Log("inactive");  
-                panel.SetActive(true);      
-                mainMenu.SetActive(true);   
-                ruleMenu.SetActive(false);
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-        }
+
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
 
     public void startWithGame()
     {
-        panel.SetActive(false);
+        SceneManager.LoadScene("UI_Scene"); // change to whatever you like 
     }
 
 
@@ -43,13 +31,12 @@ public class GameManagerScript : MonoBehaviour
     public void Rule()
     {
         ruleMenu.SetActive(true);
-        mainMenu.SetActive(false);
     }
 
     public void closeRule()
     {
         ruleMenu.SetActive(false);
-        mainMenu.SetActive(true);
+
     }
 
     public void quit()
